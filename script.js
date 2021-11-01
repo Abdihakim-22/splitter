779e3c01149a427926ca54f3b778d6927b849c9e
Abdihakim-22 <abdi911@live.com>
2021-11-01 14:38:41 +0000

    adding random colours to each boxes

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 "use strict"
 
-alert("hello")
+
+
+function split4(){
+    //splits a div into 4 by adding 4 quarters
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+
+    //rgb()function
+    //math.random
+}
+
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div") //create a 'div' element - and hold a 'pointer' to it in the variable 'q'
+    
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+
+    
+    //Add the two classes 'quarter' and flexy' to this new element 
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click",split4)
+
+    intoDiv.appendChild(q)
+
+
+}
+
+addQuarter(document.body)
+
+document.body.addEventListener("click",split4)
